4681fca445644140954562c13e204cc7c1bd9699
Artem Denisov <lifeinplus@gmail.com>
2022-08-30 17:56:31 +0300

    Display Requests with badges and highlight Microcredit Requests

--- src/components/RequestCounts.jsx
+++ src/components/RequestCounts.jsx
@@ -5,17 +5,25 @@ import { Match } from "../utils";
 const RequestCounts = (props) => {
     const { t } = useTranslation(["personal_data"]);
 
+    const values = Object.values(props.microcreditRequestsCounts);
+    const borderDanger = values.some((item) => item) && "border-danger";
+
     return (
         <div className="card-group">
             <div className="card">
-                <div className="card-header">{t("requests.title_all")}</div>
+                <div className="card-header text-center">
+                    {t("requests.title_all")}
+                </div>
                 <ListGroup counts={props.requestsCounts} type="all" />
-                <div className="card-footer">{`${t("score")}: ${
-                    props.score
-                }`}</div>
+                <div className="card-footer text-center">
+                    {t("score")}
+                    <span className="badge rounded-pill text-bg-success ms-2">
+                        {props.score}
+                    </span>
+                </div>
             </div>
-            <div className="card border-danger">
-                <div className="card-header">
+            <div className={`card ${borderDanger}`}>
+                <div className="card-header text-center">
                     {t("requests.title_microcredits")}
                 </div>
                 <ListGroup
@@ -31,14 +39,36 @@ const RequestCounts = (props) => {
             <ul className="list-group list-group-flush">
                 {Match.requestCounts
                     .filter((item) => item.type === props.type)
-                    .map(({ sysName }) => (
-                        <li key={sysName} className="list-group-item">
-                            {`${t(`requests.${sysName}`)}: ${
-                                props.counts[sysName]
-                            }`}
-                        </li>
-                    ))}
+                    .map(({ sysName }) => {
+                        return (
+                            <ListGroupItem
+                                key={sysName}
+                                counts={props.counts}
+                                sysName={sysName}
+                                type={props.type}
+                            />
+                        );
+                    })}
             </ul>
+        );
+    }
+
+    function ListGroupItem(props) {
+        const counts = props.counts[props.sysName];
+        const isDanger = props.type === "micro" && counts;
+
+        const itemDanger = isDanger && "list-group-item-danger";
+        const badgeBg = isDanger ? "danger" : "primary";
+
+        return (
+            <li
+                className={`list-group-item d-flex justify-content-between align-items-center ${itemDanger}`}
+            >
+                {t(`requests.${props.sysName}`)}
+                <span className={`badge rounded-pill text-bg-${badgeBg}`}>
+                    {counts}
+                </span>
+            </li>
         );
     }
 };
