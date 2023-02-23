5eda7b1beda620b8932e8cee8233f9c03c6ddbbd
Artem Denisov <lifeinplus@gmail.com>
2023-02-23 18:02:43 +0300

    DEV-14 Displayed statuses do not match dates

--- src/util/index.js
+++ src/util/index.js
@@ -10,8 +10,18 @@ function formatHeader(isoDate) {
     return format(date, "dd-MM-yyyy HH:mm:ss");
 }
 
+function formatToMonthYear(isoDate) {
+    const milliseconds = Date.parse(isoDate);
+
+    return new Intl.DateTimeFormat("ru", {
+        month: "numeric",
+        year: "numeric",
+        timeZone: "Europe/Moscow",
+    }).format(milliseconds);
+}
+
 function joinClasses(classes) {
     return classes.filter((item) => item).join(" ");
 }
 
-export { formatHeader, joinClasses, lngs };
+export { formatHeader, formatToMonthYear, joinClasses, lngs };
