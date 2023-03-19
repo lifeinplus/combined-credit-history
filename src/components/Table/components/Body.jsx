9cd4ede354ad02f61a2adafae987b4203db41bc9
Artem Denisov <lifeinplus@gmail.com>
2023-03-19 23:56:03 +0300

    DEV-25 Highlighting differences in personal data

--- src/components/Table/components/Body.jsx
+++ src/components/Table/components/Body.jsx
@@ -4,13 +4,14 @@ import { nanoid } from "nanoid";
 
 import { getDateTimeFormat, langs } from "../../../util";
 
-const Body = ({ columns, data, rowActive }) => {
+const Body = ({ columns, data, rowActive, textDifference }) => {
     const [activeRowId, setActiveRowId] = useState(undefined);
 
     const { i18n } = useTranslation();
     const lang = langs[i18n.resolvedLanguage];
     const numberFormat = new Intl.NumberFormat(lang.locale);
 
+    const firstDataItem = data.length ? data[0] : {};
     const statusFormat = getDateTimeFormat("ru", "tableStatus");
     const tableFormat = getDateTimeFormat(lang.locale, "table");
 
@@ -53,41 +54,47 @@ const Body = ({ columns, data, rowActive }) => {
         );
     }
 
-    function Td(params) {
-        const { type } = params.column;
-        return type === "common" || !type
-            ? getCommonTd(params)
-            : getStatusTd(params);
+    function Td({ column, data }) {
+        const { type } = column;
+
+        return type === "common" || !type ? (
+            <Common column={column} data={data} />
+        ) : (
+            <Status column={column} data={data} />
+        );
     }
 
-    function getCommonTd({ column, data }) {
-        const { alignment, dataType, sysName, sysNameStatus } = column;
+    function Common({ column, data }) {
+        const { alignment, dataType } = column;
         const { badgeEqual, badgeMore, badgeType } = column;
+        const { sysName, sysNameStatus } = column;
 
-        let value = data[sysNameStatus] ?? data[sysName] ?? "";
+        const currentSourceValue = data[sysNameStatus] ?? data[sysName] ?? "";
 
         const classNameSpan =
-            value > badgeMore || value === badgeEqual
-                ? `badge text-bg-${badgeType} badge-loans-data`
-                : "";
+            currentSourceValue > badgeMore || currentSourceValue === badgeEqual
+                ? `cch-badge cch-text-bg-${badgeType}`
+                : undefined;
 
-        if (dataType === "amount" && !isNaN(value)) {
-            value = numberFormat.format(value);
-        }
+        const firstSourceValue = firstDataItem[sysName];
 
-        if (dataType === "date" && value) {
-            const milliseconds = Date.parse(value);
-            value = tableFormat.format(milliseconds);
-        }
+        const firstValue = prepare(firstSourceValue, dataType);
+        const currentValue = prepare(currentSourceValue, dataType);
+
+        const comparedValue = textDifference
+            ? compare(firstValue, currentValue)
+            : undefined;
 
         return (
             <td className={alignment}>
-                <span className={classNameSpan}>{value}</span>
+                <span className={classNameSpan}>
+                    {comparedValue || currentValue}
+                </span>
             </td>
         );
     }
 
-    function getStatusTd({ column, data }) {
+    function Status({ column, data }) {
         const { name } = column;
         const { MonthlyHistoryList } = data;
 
@@ -95,13 +102,97 @@ const Body = ({ columns, data, rowActive }) => {
             return item.name === name ? item.AccountPaymentStatus : result;
         }, null);
 
-        const classNameSpan = value ? `cch-badge cch-text-bg-${value}` : "";
+        const classNameSpan = value
+            ? `cch-badge cch-status cch-text-bg-${value}`
+            : "";
 
         return (
-            <td className={"cch-status"}>
+            <td className={"cch-status-td"}>
                 <span className={classNameSpan}>{value}</span>
             </td>
         );
+    }
+
+    function prepare(sourceValue, dataType) {
+        if (dataType === "date" && sourceValue) {
+            const milliseconds = Date.parse(sourceValue);
+            return tableFormat.format(milliseconds);
+        }
+
+        if (dataType === "amount" && !isNaN(sourceValue)) {
+            return numberFormat.format(sourceValue);
+        }
+
+        return sourceValue;
+    }
+
+    function compare(valueA, valueB) {
+        let result = [];
+
+        const arrayA = valueA.split(" ");
+        const arrayB = valueB.split(" ");
+
+        const maxLength = Math.max(arrayA.length, arrayB.length);
+
+        for (let i = 0; i < maxLength; i++) {
+            const stringA = arrayA[i] || "";
+            const stringB = arrayB[i] || "";
+
+            let compared = compareStrings(stringA, stringB);
+
+            if (i < maxLength - 1) {
+                compared = compared.concat([{ text: " " }]);
+            }
+
+            result = result.concat(compared);
+        }
+
+        return result.map(({ spanText, text }) => {
+            return spanText ? (
+                <span
+                    key={nanoid()}
+                    className={"cch-badge cch-diff cch-text-bg-A"}
+                >
+                    {spanText}
+                </span>
+            ) : (
+                text
+            );
+        });
+    }
+
+    function compareStrings(stringA, stringB) {
+        let result = [];
+        let text = "";
+        let spanText = "";
+        const maxLength = Math.max(stringA.length, stringB.length);
+
+        for (let i = 0; i < maxLength; i++) {
+            const charA = stringA[i] || "";
+            const charB = stringB[i] || "";
+
+            if (charA !== charB && charB) {
+                if (text) {
+                    result.push({ text });
+                    text = "";
+                }
+
+                spanText += charB;
+                continue;
+            }
+
+            if (spanText) {
+                result.push({ spanText });
+                spanText = "";
+            }
+
+            text += charB;
+        }
+
+        if (spanText) result.push({ spanText });
+        if (text) result.push({ text });
+
+        return result;
     }
 };
 
